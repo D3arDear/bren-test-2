627c67e691e2e2ac4a229ddbf3e337dbe09c1665
ex.arc.zhang@gmail.com <ex.arc.zhang@gmail.com>
2019-12-23 20:50:43 +0800

    webpack-dev-server 自动打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+// 由于需要绝对路径所以用这个 path
 module.exports = {
   mode: "production",
   entry: {
     bui: "./lib/index.tsx"
   },
   output: {
+    // 输出到哪
     path: path.resolve(__dirname, "dist/lib"),
     library: "BUI",
     libraryTarget: "umd"
